4d25d14954ae787e7406ec70e12a51bcb47513e7
max <none@none.com>
2020-03-22 15:40:41 -0600

    testing if this commit is signed...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+//
 // N64 emulator written by Max Hanson March 2020 -> (present)
 // note: in comments, 'datasheet' refers to the NEC VR4300 user's manual
 //   included in the documentation in this repository
+//
 
 
 mod cpu;
